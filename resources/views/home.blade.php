91918d5f456c4a7b5b102abc37de3a714f68a259
AngelManuelGS <jaba.manuel@gmail.com>
2025-02-06 22:40:33 -0600

    Co-authored-by: DenisseRocio <DenisseRocio@users.noreply.github.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mx-auto mt-5">
-    <h1 class="text-2xl font-bold">¡Bienvenido a IEBO!</h1>
-    <p class="mt-3 text-gray-600">Esta es la página principal después de iniciar sesión.</p>
-</div>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <!-- Carrusel Bootstrap -->
+            <div id="carouselExampleIndicators" class="carousel slide shadow-lg rounded" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></button>
+                </div>
+                <div class="carousel-inner rounded">
+                    <div class="carousel-item active">
+                        <img src="{{ asset('img/Marco2.png') }}" class="d-block w-100" alt="Imagen 1">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('img/Marco2.png') }}" class="d-block w-100" alt="Imagen 2">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('img/Marco2.png') }}" class="d-block w-100" alt="Imagen 3">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                </button>
+            </div>
+        </div>
+    </div>
+@stop
+@section('css')
+    <style>
+        .carousel {
+            max-height: 350px; /* Ajusta la altura del carrusel */
+        }
+        .carousel img {
+            object-fit: cover;
+            max-height: 350px;
+        }
+    </style>
+@stop
+
 @endsection
